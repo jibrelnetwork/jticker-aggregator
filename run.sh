f6147392afebebf0d8ee8ca6b10d7de76980c58e
Roman Tolkachyov <roman@tolkachyov.name>
2019-03-25 13:26:11 +0300

    ptw, testmon integration

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ if [ "${RUNMODE}" = "app" ]; then
     python -m jticker_aggregator
 elif [ "${RUNMODE}" = "test" ]; then
     pytest "${@:5}"
+elif [ "${RUNMODE}" = "ptw" ]; then
+    ptw "${@:4}"
 else
     python manage.py "$@"
 fi
